311f97daf26ef23c340d9a8e8dde2ff8b12db1d3
301choso <jsnn5279@naver.com>
2023-03-02 01:42:05 +0900

    db설정 추가, 포트번호 설정

--- src/main/java/kr/rebe/deal/DealApplication.java
+++ src/main/java/kr/rebe/deal/DealApplication.java
@@ -2,8 +2,10 @@ package kr.rebe.deal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:/database.properties")
 public class DealApplication {
 
 	public static void main(String[] args) {
