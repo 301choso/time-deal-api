7a5ee2cfb3a0b6548544159515e7e7dc33a22d75
301choso <jsnn5279@naver.com>
2023-03-08 17:46:04 +0900

    상품 조회, 등록, 수정, 삭제 기능 추가

---
+++ src/main/java/kr/rebe/deal/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package kr.rebe.deal.repository;
+
+import kr.rebe.deal.entity.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
