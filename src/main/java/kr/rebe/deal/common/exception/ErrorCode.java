5bb159fec90ddbb5ade69be0ad9bea3dffb426f2
301choso <jsnn5279@naver.com>
2023-03-08 17:46:04 +0900

    상품 조회, 등록, 수정, 삭제 기능 추가

--- src/main/java/kr/rebe/deal/common/exception/ErrorCode.java
+++ src/main/java/kr/rebe/deal/common/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    UNAUTHORIZED_MEMBER(HttpStatus.BAD_REQUEST, "아이디 비밀번호가 올바르지 않거나 회원이 아닙니다.");
+    UNAUTHORIZED_MEMBER(HttpStatus.BAD_REQUEST, "아이디 비밀번호가 올바르지 않거나 회원이 아닙니다."),
+    NOT_EXIST_PRODUCT(HttpStatus.NOT_FOUND, "제품이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
---
+++ src/main/java/kr/rebe/deal/entity/Product.java
@@ -0,0 +1,61 @@
+package kr.rebe.deal.entity;
+
+import kr.rebe.deal.dto.ProductDto;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productSeq;
+
+    private String productName;
+
+    private Long price;
+
+    private Long discount;
+
+    private Long stock;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    private LocalDateTime regDate;
+
+    @Builder
+    public Product(Long productSeq, String productName, Long price, Long discount, Long stock, LocalDateTime startDate, LocalDateTime endDate, LocalDateTime regDate) {
+        this.productSeq = productSeq;
+        this.productName = productName;
+        this.price = price;
+        this.discount = discount;
+        this.stock = stock;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.regDate = regDate;
+    }
+
+    public ProductDto toDto() {
+        return ProductDto.builder()
+                .productSeq(productSeq)
+                .productName(productName)
+                .price(price)
+                .discount(discount)
+                .stock(stock)
+                .startDate(startDate)
+                .endDate(endDate)
+                .regDate(LocalDateTime.now())
+                .build();
+    }
+}
